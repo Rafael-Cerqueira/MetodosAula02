8534048aba19e30c27dbf05c9ac65c6c52b91951
Rafael Cerqueira <rafael.scerqueira@hotmail.com>
2022-05-30 22:04:48 -0300

    Initial commit

---
+++ src/one/dio/basecamp/Main.java
@@ -0,0 +1,14 @@
+package one.dio.basecamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Quadrilatero
+        System.out.println("Exercicio Quadrilatero: ");
+        Quadrilatero.area(4);
+        Quadrilatero.area(4,7);
+        Quadrilatero.area(4,8,10);
+
+    }
+}
